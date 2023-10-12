8234a6c822559d23c1d09ef2d161ba55bf183bb4
Duduhiro <duduhiro0312@gmail.com>
2023-10-12 02:59:55 -0300

    Solved ex02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat) {
+    ans = ''
+    if (repeat < 0) {
+        return 'ERROR'
+    }
+    for (let i = 0; i < repeat; i++) {
+        ans += word
+    }
+    return ans
 };
 
 // Do not edit below this line
