5fbea7a4751195d6c8407bcf69d4d0a02e916a4e
Duduhiro <duduhiro0312@gmail.com>
2023-10-12 03:05:18 -0300

    Solved ex03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    ans = ''
+    for (let i = phrase.length - 1; i > -1; i--) {
+        ans += phrase[i]
+    }
+    return ans
 };
 
 // Do not edit below this line
