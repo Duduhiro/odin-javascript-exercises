639b4da2b18935ab875e61cfa02ddd40bc19b4a0
Duduhiro <duduhiro0312@gmail.com>
2023-10-12 22:50:14 -0300

    Solved ex05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a > b) {
+        let temp = b
+        b = a
+        a = temp
+    }
 
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+
+    if (typeof(a) != 'number' || typeof(b) != 'number') {
+        return "ERROR"
+    }
+
+    // Sum of arithmetic progression
+    ans = ((b - a + 1) / 2) * (a + b)
+    return ans
 };
 
 // Do not edit below this line
