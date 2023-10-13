c66671bf5454dbaf9d5d87f71e1771342d17ce8e
Duduhiro <duduhiro0312@gmail.com>
2023-10-12 22:41:25 -0300

    Solved ex04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    
+    // We are using a list to store all the numbers to be removed from the array
+    let toRemove = []
+    
+    // Takes all the arguments provided except for the first one, because it's the array
+    for (let i = 1; i < arguments.length; i++) {
+        toRemove.push(arguments[i])
+    }
+
+    let pointer = 0
+    let removed = false
+    while (pointer < arr.length) {
+        
+        removed = false
+        if (pointer == arr.length) {
+            break
+        }
+
+        toRemove.forEach(element => {
+            if (element === arr[pointer]) {
+                arr.splice(pointer, 1)
+                removed = true
+            }
+        });
+        if (!removed) {
+            pointer++
+        }
+    }
+    console.log(arr)
+    return arr
 
 };
 
