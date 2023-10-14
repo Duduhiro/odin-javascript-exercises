0d4c115f7eb053a67d5bee2887c5e8fa8ab6e954
Duduhiro <duduhiro0312@gmail.com>
2023-10-14 18:36:43 -0300

    Solved ex10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    
+    if (n < 0 ) {
+        return "OOPS"
+    }
+    
+    let sequence = [1, 1]
+    let value = 0
+    for (let i = 2; i < n; i++) {
+        value = sequence[i - 2] + sequence[i - 1]
+        sequence.push(value)
+    }
+    console.log(sequence)
+    return sequence[n - 1]
 };
 
 // Do not edit below this line
