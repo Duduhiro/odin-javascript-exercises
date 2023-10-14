255b549b7c69541516376001779833e5fe004fc9
Duduhiro <duduhiro0312@gmail.com>
2023-10-14 19:03:59 -0300

    Didn't solved one of the three tests from ex12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0
+    let age = 0
+    let oldest = {
+        'name': 'None'
+    }
+    people.forEach(person => {
+        
+        age = person.yearOfDeath - person.yearOfBirth
+        
+        if (age > oldestAge) {
+            oldestAge = age
+            oldest.name = person.name
+        }
+    });
+    return oldest
 };
 
 // Do not edit below this line
