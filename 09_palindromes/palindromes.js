ca2bb4ea70b568116b8f263b8e2a0289e2942afb
Duduhiro <duduhiro0312@gmail.com>
2023-10-14 18:30:57 -0300

    Solved ex09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    
+    let skipper = 0
+    let end_skipper = 0
 
+    s = s.toLowerCase()
+    s = s.replaceAll(' ', '')
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i + skipper] === '!' || s[i + skipper] === ',' || s[i + skipper] === '.' || s[i + skipper] === '?') {
+            skipper++
+        }
+        if (s[s.length - i - 1 - end_skipper] === '!' || s[s.length - i - 1 - end_skipper] === ',' || s[s.length - i - 1 - end_skipper] === '.' || s[s.length - i - 1 - end_skipper] === '?') {
+            end_skipper++
+        }
+        if (s[i + skipper] != s[s.length - i - 1 - end_skipper]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
